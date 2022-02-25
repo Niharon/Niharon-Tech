76cc77a8924d84588cc81d39e98ed8770d5a4e11
muhammad zeon <73699852+zeon-X@users.noreply.github.com>
2022-02-26 05:01:52 +0600

    first commit

---
+++ js/contact-us.js
@@ -0,0 +1,32 @@
+function sendEmail() {
+
+    var temParamsx = {
+        to_name: "webdesk inc",
+        from_name: document.getElementById("firstName").value + " " + document.getElementById("lastName").value,
+        message: "phone : " + document.getElementById("Phone_no").value + " email : " + document.getElementById("email_id").value + " Message : " + document.getElementById("msg").value,
+    };
+
+
+
+    emailjs.send('service_u3shbvj', 'template_pw0963s', temParamsx, 'user_tddYoZg4Dc7m2gTSX0C0C')
+        .then(function () {
+            swal("Done!", "Message Sent Successfully!", "success");
+        }, function () {
+            swal("Opps!", "Something Went Wrong", "warning");
+        });
+
+
+    // console.log(temParamsx);//
+    document.getElementById("cntForm").reset();
+
+
+}
+
+// window.onload = function () {
+document.getElementById("cnt-submit-btn").
+    addEventListener('click', () => {
+        sendEmail();
+    })
+
+// }
+
