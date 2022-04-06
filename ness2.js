60a8e8660fc24e4ee454430f90ba384575662535
Sakhawat Hossain <96255034+MSHRSAKHAWAT@users.noreply.github.com>
2022-04-06 19:54:23 +0600

    nav bg prblm
    bg ase na scrl korle

--- ness2.js
+++ ness2.js
@@ -8,7 +8,8 @@ function abc(x) {
 <div class="container-fluid">
         <div class="row">
           <div class="col-lg-12 d-flex justify-content-center align-items-center">
-            <nav class="navbar navbar-expand-lg navbar-dark nav_float">
+            <nav class="navbar navbar-expand-lg navbar-dark nav_float fixed-top bgcolor">
+
               <a href="#" class=""><img width="150" src="../images/logo/Niharon Logo Renew-02.png" alt="no img"/></a>
 
               <button class="navbar-toggler"type="button"data-bs-toggle="collapse"data-bs-target="#navbarSupportedContent"aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
