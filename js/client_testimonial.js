49593c282e11022bd365527bd31061b33fec9149
Abduz Zami <69592754+AbduzZami@users.noreply.github.com>
2022-09-12 01:09:40 +0600

    js fix

--- js/client_testimonial.js
+++ js/client_testimonial.js
@@ -11,9 +11,15 @@ const testimonials = [
     photo: "images/client_testimonial/riad_hasan.jpg",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem repellendus inventore hic quo ipsum nihil.Incidunt qui ipsum quisquam sequi maxime architecto similique reiciendis quidem facilis corporis libero nam nemo ratione id necessitatibus ab debitis nulla harum.Optio corrupti dolorum debitis incidunt est architecto voluptas aut nobis amet corporis accusamus.",
   },
+  {
+    name: "MD. Rakib Hasnat Biswas",
+    position: "Founder, Ahoron ",
+    photo: "images/client_testimonial/rakib_hasnat.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem repellendus inventore hic quo ipsum nihil.Incidunt qui ipsum quisquam sequi maxime architecto similique reiciendis quidem facilis corporis libero nam nemo ratione id necessitatibus ab debitis nulla harum.Optio corrupti dolorum debitis incidunt est architecto voluptas aut nobis amet corporis accusamus.",
+  },
 ];
 
-let idx = 1;
+let idx = 0;
 
 function upadteTestimonial() {
   const { name, position, photo, text } = testimonials[idx];
