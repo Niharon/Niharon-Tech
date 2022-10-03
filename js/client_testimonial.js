c81227b69fc60912e7dddecfddc3d17a9ec0e58c
Sakhawat Hossain <96255034+MSHRSAKHAWAT@users.noreply.github.com>
2022-10-03 16:39:44 +0600

    Revert "push index"
    This reverts commit 2fbade92423fff45207a29502a545f49caf2726f.

--- js/client_testimonial.js
+++ js/client_testimonial.js
@@ -1,24 +1,54 @@
-$(".slick_carosole").slick({
-  nextArrow: '<span class="priv_arrow"><i class="fa fa-angle-left less_then"></i></span>',
-  prevArrow: '<span class="next_arrow"><i class="fa fa-angle-right gater_then"></i></span>',
-  autoplay: false,
-  autoplaySpeed: 2500,
-  dots: false,
-  arrows: true,
-  infinite: true,
-  speed: 500,
-  cssEase: "linear",
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 700,
-      settings: {
-        dots: false,
-        arrows: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+$(document).ready(function () {
+  $(".icones-carousel").slick({
+    nextArrow: '<span class="priv_arrow"><i class="fa fa-angle-left less_then"></i></span>',
+    prevArrow: '<span class="next_arrow"><i class="fa fa-angle-right gater_then"></i></span>',
+    autoplay: true,
+    autoplaySpeed: 2500,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    cssEase: "linear",
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          dots: false,
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
       },
-    },
-  ],
+      {
+        breakpoint: 800,
+        settings: {
+          dots: false,
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 630,
+        settings: {
+          dots: true,
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          autoplay: false,
+          dots: true,
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
