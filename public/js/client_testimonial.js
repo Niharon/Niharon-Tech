b89811ce52526cfd87375303b0b67332962e5475
Abduz Zami <69592754+AbduzZami@users.noreply.github.com>
2022-10-05 16:35:10 +0600

    hosted

--- public/js/client_testimonial.js
+++ public/js/client_testimonial.js
@@ -1,54 +1,156 @@
-$(document).ready(function () {
-  $(".icones-carousel").slick({
-    nextArrow: '<span class="priv_arrow"><i class="fa fa-angle-left less_then"></i></span>',
-    prevArrow: '<span class="next_arrow"><i class="fa fa-angle-right gater_then"></i></span>',
-    autoplay: true,
-    autoplaySpeed: 2500,
-    dots: false,
-    arrows: true,
-    infinite: true,
-    speed: 500,
-    cssEase: "linear",
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1300,
-        settings: {
-          dots: false,
-          arrows: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 800,
-        settings: {
-          dots: false,
-          arrows: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 630,
-        settings: {
-          dots: true,
-          arrows: false,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 575,
-        settings: {
-          autoplay: false,
-          dots: true,
-          arrows: false,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+const reviewWrap = document.getElementById("reviewWrap");
+const leftArrow = document.getElementById("leftArrow");
+const rightArrow = document.getElementById("rightArrow");
+const imgDiv = document.getElementById("imgDiv");
+const personName = document.getElementById("personName");
+const profession = document.getElementById("profession");
+const description = document.getElementById("description");
+const surpriseMeBtn = document.getElementById("surpriseMeBtn");
+const chicken = document.querySelector(".chicken");
+
+let isChickenVisible;
+
+let people = [
+  {
+    photo: 'url("images/client_testimonial/rakib_hasnat.jpg")',
+    name: "Md. Rakibul Biswas",
+    profession: "Founder, Ahoron",
+    description:
+      "We have been very happy with our new website! It looks professional and very easy to navigate. I would highly recommend Niharon Technology. They are great to work with.",
+  },
+
+  {
+    photo: 'url("images/client_testimonial/riad_hasan.jpg")',
+    name: "Md. Riad Hasan",
+    profession: "Founder, Softology",
+    description:
+      "It was an amazing deal. Your work is so professional. I am happy with that. Next time, I will go with you without any thought. Thank you.",
+  },
+
+  {
+    photo: 'url("images/client_testimonial/rakib_hasnat.jpg")',
+    name: "Md. Rakibul Biswas",
+    profession: "Founder, Ahoron",
+    description:
+      "We have been very happy with our new website! It looks professional and very easy to navigate. I would highly recommend Niharon Technology. They are great to work with.",
+  },
+
+  {
+    photo: 'url("images/client_testimonial/riad_hasan.jpg")',
+    name: "Md. Riad Hasan",
+    profession: "Founder, Softology",
+    description:
+      "It was an amazing deal. Your work is so professional. I am happy with that. Next time, I will go with you without any thought. Thank you.",
+  },
+];
+
+imgDiv.style.backgroundImage = people[0].photo;
+personName.innerText = people[0].name;
+profession.innerText = people[0].profession;
+description.innerText = people[0].description;
+let currentPerson = 0;
+
+//Select the side where you want to slide
+function slide(whichSide, personNumber) {
+  let reviewWrapWidth = reviewWrap.offsetWidth + "px";
+  let descriptionHeight = description.offsetHeight + "px";
+  //(+ or -)
+  let side1symbol = whichSide === "left" ? "" : "-";
+  let side2symbol = whichSide === "left" ? "-" : "";
+
+  let tl = gsap.timeline();
+
+  if (isChickenVisible) {
+    tl.to(chicken, {
+      duration: 0.4,
+      opacity: 0,
+    });
+  }
+
+  tl.to(reviewWrap, {
+    duration: 0.4,
+    opacity: 0,
+    translateX: `${side1symbol + reviewWrapWidth}`,
   });
+
+  tl.to(reviewWrap, {
+    duration: 0,
+    translateX: `${side2symbol + reviewWrapWidth}`,
+  });
+
+  setTimeout(() => {
+    imgDiv.style.backgroundImage = people[personNumber].photo;
+  }, 400);
+  setTimeout(() => {
+    description.style.height = descriptionHeight;
+  }, 400);
+  setTimeout(() => {
+    personName.innerText = people[personNumber].name;
+  }, 400);
+  setTimeout(() => {
+    profession.innerText = people[personNumber].profession;
+  }, 400);
+  setTimeout(() => {
+    description.innerText = people[personNumber].description;
+  }, 400);
+
+  tl.to(reviewWrap, {
+    duration: 0.4,
+    opacity: 1,
+    translateX: 0,
+  });
+
+  if (isChickenVisible) {
+    tl.to(chicken, {
+      duration: 0.4,
+      opacity: 1,
+    });
+  }
+}
+
+function setNextCardLeft() {
+  if (currentPerson === 3) {
+    currentPerson = 0;
+    slide("left", currentPerson);
+  } else {
+    currentPerson++;
+  }
+
+  slide("left", currentPerson);
+}
+
+function setNextCardRight() {
+  if (currentPerson === 0) {
+    currentPerson = 3;
+    slide("right", currentPerson);
+  } else {
+    currentPerson--;
+  }
+
+  slide("right", currentPerson);
+}
+
+leftArrow.addEventListener("click", setNextCardLeft);
+rightArrow.addEventListener("click", setNextCardRight);
+
+surpriseMeBtn.addEventListener("click", () => {
+  if (chicken.style.opacity === "0") {
+    chicken.style.opacity = "1";
+    imgDiv.classList.add("move-head");
+    surpriseMeBtn.innerText = "Remove the chicken";
+    surpriseMeBtn.classList.remove("surprise-me-btn");
+    surpriseMeBtn.classList.add("hide-chicken-btn");
+    isChickenVisible = true;
+  } else if (chicken.style.opacity === "1") {
+    chicken.style.opacity = "0";
+    imgDiv.classList.remove("move-head");
+    surpriseMeBtn.innerText = "Surprise me";
+    surpriseMeBtn.classList.add("surprise-me-btn");
+    surpriseMeBtn.classList.remove("hide-chicken-btn");
+    isChickenVisible = false;
+  }
+});
+
+window.addEventListener("resize", () => {
+  description.style.height = "100%";
 });
